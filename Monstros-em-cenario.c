8e95b665144ae58c5b1b5ad2598f11d58fc8991a
IgorLekal <igorgac2004@gmail.com>
2023-09-20 05:03:29 -0300

    Testagem

---
+++ Monstros-em-cenario.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (void)
+{
+    int valor = 100;
+
+    printf("Ola, Sentinela!!");
+    scanf("%d", &valor);
+    printf("Valor: %d\n", valor);
+    return EXIT_SUCCESS;
+}
