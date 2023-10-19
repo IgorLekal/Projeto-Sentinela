db4ad729fa3b179653b226da94d19d8d5bb6258e
Igor Gabriel Cabral De Oliveira <igco@poli.br>
2023-10-19 01:14:57 -0300

    Iniciada base do codigo: Dano sendo recebido

--- Monstros-em-cenario.c
+++ Monstros-em-cenario.c
@@ -1,12 +1,59 @@
+// =-= // Includes // =-= //
+
 #include <stdlib.h>
 #include <stdio.h>
 
-int main (void)
-{
-    int valor = 100;
+// =-= // Declacarao de Structs // =-= //
 
-    printf("Ola, Sentinela!!");
-    scanf("%d", &valor);
-    printf("Valor: %d\n", valor);
+// Status de um Inimigo no Cenario
+struct StatusInimigo
+{
+    char nome[15];
+    int vida;
+
+} Inimigo;
+
+// Dano Aplicado a um Inimigo
+struct DanoAplicado
+{
+    int dano;
+} Dano;
+
+// =-= // Declacarao de Funcoes // =-= //
+
+int DanoDesferido(void);
+int DanoSofrido(void);
+
+// =-= // Funcao Principal // =-= //
+
+int main(void)
+{
+    printf("Digite o nome do inimigo: ");
+    scanf("%s", Inimigo.nome);
+
+    printf("Digite a vida do inimigo: ");
+    scanf("%d", &Inimigo.vida);
+
+    DanoDesferido();
+    DanoSofrido();
+
+    printf("Vida atual de %s: %d\n", Inimigo.nome, Inimigo.vida);
     return EXIT_SUCCESS;
+}
+
+// =-=-=-=-=-=-= Funcao de Dano Desferido =-=-=-=-=-=-= //
+
+int DanoDesferido(void)
+{
+    printf("\nDano recebido: ");
+    scanf("%d", &Dano.dano);
+    return Dano.dano;
+}
+
+// =-=-=-=-=-=-= Funcao de Dano Sofrido =-=-=-=-=-=-= //
+
+int DanoSofrido(void)
+{
+    Inimigo.vida = Inimigo.vida - Dano.dano;
+    return Inimigo.vida;
 }
